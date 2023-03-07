e0ba7486641638bfccfe22c5fa51badc80e1436e
eduardsdn <eduard.sdn@gmail.com>
2023-03-07 14:37:51 +0100

    Finished styling for menu section

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import './style.css'
 import createHeader from "./modules/loadpage.js"
 import createHome from "./modules/home.js"
 
-const content = document.querySelector(".content")
-content.appendChild(createHeader())
-content.appendChild(createHome())
+// const content = document.querySelector(".content")
+// content.appendChild(createHeader())
+// content.appendChild(createHome())
